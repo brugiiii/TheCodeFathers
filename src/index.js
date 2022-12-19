98096155e26de7b0a9654161b341dc634d1aed38
Eduard Bruhosh <edyabrug91@gmail.com>
2022-12-19 06:01:05 +0200

    123

---
+++ src/index.js
@@ -0,0 +1,138 @@
+// Read more modal
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[read-more-modal-open]'),
+    closeModalBtn: document.querySelector('[read-more-modal-close]'),
+    modal: document.querySelector('[read-more-modal]'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+  }
+})();
+
+// our locotion modal
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[location-modal-open]'),
+    closeModalBtn: document.querySelector('[location-modal-close]'),
+    modal: document.querySelector('[location-modal]'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+  }
+})();
+
+// franchise modal
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[franchise-modal-open]'),
+    closeModalBtn: document.querySelector('[franchise-modal-close]'),
+    modal: document.querySelector('[franchise-modal]'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+  }
+})();
+
+// product info madal
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[prod-modal-open]'),
+    closeModalBtn: document.querySelector('[prod-modal-close]'),
+    modal: document.querySelector('[prod-modal]'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+  }
+})();
+
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[prod-modal-open2]'),
+    closeModalBtn: document.querySelector('[prod-modal-close]'),
+    modal: document.querySelector('[prod-modal]'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+  }
+})();
+
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[prod-modal-open3]'),
+    closeModalBtn: document.querySelector('[prod-modal-close]'),
+    modal: document.querySelector('[prod-modal]'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+  }
+})();
+
+// header scroll
+window.onscroll = () => changeHeaderBackground();
+
+function changeHeaderBackground() {
+  const header = document.querySelector('[data-header]');
+  const headerOffsetTrigger = header.offsetHeight;
+  const pageOffset = window.pageYOffset;
+
+  if (pageOffset > headerOffsetTrigger) {
+    header.classList.add('header--transparent');
+  } else {
+    header.classList.remove('header--transparent');
+  }
+}
+
+// mobile menu
+(() => {
+  const mobileMenu = document.querySelector('.js-menu-container');
+  const openMenuBtn = document.querySelector('.js-open-menu');
+  const closeMenuBtn = document.querySelector('.js-close-menu');
+
+  const toggleMenu = () => {
+    const isMenuOpen =
+      openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
+    openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
+    mobileMenu.classList.toggle('is-open');
+
+    const scrollLockMethod = !isMenuOpen
+      ? 'disableBodyScroll'
+      : 'enableBodyScroll';
+    bodyScrollLock[scrollLockMethod](document.body);
+  };
+
+  openMenuBtn.addEventListener('click', toggleMenu);
+  closeMenuBtn.addEventListener('click', toggleMenu);
+
+  // Close the mobile menu on wider screens if the device orientation changes
+  window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
+    if (!e.matches) return;
+    mobileMenu.classList.remove('is-open');
+    openMenuBtn.setAttribute('aria-expanded', false);
+    bodyScrollLock.enableBodyScroll(document.body);
+  });
+})();
