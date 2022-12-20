e2137255b104c2a171aea470bb2f9080a495585e
Eduard <edyabrug91@gmail.com>
2022-12-20 21:52:53 +0200

    done

--- src/index.js
+++ src/index.js
@@ -167,6 +167,10 @@ function changeHeaderBackground() {
   const mobileMenu = document.querySelector('.js-menu-container');
   const openMenuBtn = document.querySelector('.js-open-menu');
   const closeMenuBtn = document.querySelector('.js-close-menu');
+  const closeMenuLink = document.querySelectorAll('.mobile-menu__link');
+  const buyNowBtn = document.querySelector('.buy-now-button--tablet-none');
+
+  
 
   const toggleMenu = () => {
     const isMenuOpen =
@@ -179,7 +183,8 @@ function changeHeaderBackground() {
       : 'enableBodyScroll';
     bodyScrollLock[scrollLockMethod](document.body);
   };
-
+closeMenuLink.forEach(item => item.addEventListener("click", toggleMenu));
+buyNowBtn.addEventListener("click", toggleMenu);
   openMenuBtn.addEventListener('click', toggleMenu);
   closeMenuBtn.addEventListener('click', toggleMenu);
 
