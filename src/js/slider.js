56762351f08ec901eb1f4c44966850999523bc48
Eduard <edyabrug91@gmail.com>
2022-12-20 11:34:24 +0200

    without slider

--- src/js/slider.js
+++
@@ -1,9 +0,0 @@
-// $('.reviews__thumb').slick({
-//     dots: true,
-//     infinite: true,
-//     speed: 500,
-//     fade: true,
-//     cssEase: 'linear',
-//     slideToShow:1,
-//     slidesToScroll:1
-//   });
