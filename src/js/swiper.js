4110fea03dc70ebfa202e90d3cdb16a6a2caec8b
Eduard Bruhosh <edyabrug91@gmail.com>
2022-12-22 21:21:11 +0200

    done

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,21 +1,18 @@
 const swiper = new Swiper('.swiper', {
   // Optional parameters
-  direction: 'vertical',
+  direction: 'horizontal',
   loop: true,
 
-  // If we need pagination
+  // pagination
   pagination: {
     el: '.swiper-pagination',
+    clickable: true,
   },
 
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
 
-  // And if we need scrollbar
+  // scrollbar
   scrollbar: {
     el: '.swiper-scrollbar',
   },
 });
+
