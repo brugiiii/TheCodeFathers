8e302dd29995fb8bd55cf41f67179e2ea9a63a82
brugiiii <113450719+brugiiii@users.noreply.github.com>
2022-12-22 19:22:42 +0000

    Deploying to gh-pages from @ brugiiii/TheCodeFathers@0b9da6c3abbc1ac47dfd316e97fc561690dea2c0 🚀

---
+++ index.bf9cdc78.js
@@ -0,0 +1,2 @@
+var swiper=new Swiper(".swiper",{direction:"horizontal",loop:!0,pagination:{el:".swiper-pagination",clickable:!0},scrollbar:{el:".swiper-scrollbar"}});
+//# sourceMappingURL=index.bf9cdc78.js.map
